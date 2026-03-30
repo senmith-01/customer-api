16a0aa651c62f8497c87854180f58f69faf65e78
senmithTheCoder <senmith01@gmail.com>
2026-03-31 01:40:53 +0530

    addign customerController and design the scetch

---
+++ controller/customerController.js
@@ -0,0 +1,11 @@
+const Customer =require ('../model/Customer');
+
+const getAllCustomers=(req,resp)=>{};
+const getCustomerById=(req,resp)=>{};
+const createNewCustomer=(req,resp)=>{};
+const updateCustomer=(req,resp)=>{};
+const deleteCustomer=(req,resp)=>{};
+
+module.exports={
+    getAllCustomers,getCustomerById,createNewCustomer,updateCustomer,deleteCustomer
+}
