591adfa9366ba48f415bba5e3e74112c8f40fe76
senmithTheCoder <senmith01@gmail.com>
2026-03-31 01:57:56 +0530

    Completed controller with delete function

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,25 +1,33 @@
 const Customer = require('../model/Customer');
 
-const getAllCustomers = (req, resp) => { 
-    resp.status(200).json(Customer.findAll());    
+const getAllCustomers = (req, resp) => {
+    resp.status(200).json(Customer.findAll());
 };
-const getCustomerById = (req, resp) => { 
-    let selectedCustomer= Customer.findById(parseInt(req.params.id)); 
-    if(!selectedCustomer) return resp.status(404).json({message: 'customer not found'});
-    resp.status(200).json(selectedCustomer);   
+
+const getCustomerById = (req, resp) => {
+    let selectedCustomer = Customer.findById(parseInt(req.params.id));
+    if (!selectedCustomer) return resp.status(404).json({ message: 'customer not found' });
+    resp.status(200).json(selectedCustomer);
 };
-const createNewCustomer = (req, resp) => { 
-    const {name , address, salary} = req.body;
-    let createdCustomer=Customer.create({name,address,salary});
+const createNewCustomer = (req, resp) => {
+    const { name, address, salary } = req.body;
+    let createdCustomer = Customer.create({ name, address, salary });
     resp.status(201).json(createdCustomer);
 };
-const updateCustomer = (req, resp) => { 
-    let id=parseInt(req.params.id);
-    Customer,update(id,resp.body);
-    if(updatedCustomer) return resp.status(404).json({message: 'Customer not found'})
-    resp.status(201).json(up)    
+
+const updateCustomer = (req, resp) => {
+    let id = parseInt(req.params.id);
+    let updatedCustomer = Customer, update(id, resp.body);
+    if (!updatedCustomer) return resp.status(404).json({ message: 'Customer not found' })
+    resp.status(201).json(updatedCustomer);
 };
-const deleteCustomer = (req, resp) => { };
+
+const deleteCustomer = (req, resp) => {
+    let id = parseInt(req.params.id);
+    let deletedCustomer = Customer.delete(id);
+    if (!deletedCustomer) return resp.status(404).json({ message: 'Customer not found' });
+    resp.status(204).json({ message });
+};
 
 module.exports = {
     getAllCustomers, getCustomerById, createNewCustomer, updateCustomer, deleteCustomer
