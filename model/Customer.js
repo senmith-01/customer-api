54f4325810e468aa3c125308fcb0aeac6cc3008a
senmithTheCoder <senmith01@gmail.com>
2026-03-31 01:31:42 +0530

    Modifiedgit status! customer.js

--- model/Customer.js
+++ model/Customer.js
@@ -9,10 +9,37 @@ class Customer {
         this.salary = salary;
     }
 
-    static create(data){
-        const customer=new Customer(data.name,data.address,data.salary);
+    static create(data) {
+        const customer = new Customer(data.name, data.address, data.salary);
         customers.push(customer);
-        return customer
+        return customer;
     }
 
+    static findAll() {
+        return customers;
+    }
+
+    static findById(id) {
+        return customers.find(c=>c.id===id);
+    }
+
+    static update (id , data) {
+        let selectedCustomer= this.findById(id);
+        if (selectedCustomer){
+            selectedCustomer.name= data.name ?? selectedCustomer.name;
+            selectedCustomer.address= data.address ?? selectedCustomer.address;
+            selectedCustomer.salary= data.salary ?? selectedCustomer.salary;
+        }
+        return selectedCustomer;
+    }
+
+    static delete(id){
+        let selectedIndex= customers.findIndex(c=>c.id===id);
+        if (selectedIndex !== -1){
+            return customers.splice(selectedIndex, 1)[0];
+        }
+        return null;
+    }
 }
+
+module.exports=Customer
