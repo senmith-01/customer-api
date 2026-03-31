efc0b6e7c7de4f7abcf0849bcad48823fad97e43
senmithTheCoder <senmith01@gmail.com>
2026-03-31 16:37:21 +0530

    added route directory

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express= require('express');
+const express = require('express');
 require('dotenv').config();
 
-const app=express();
+const app = express();
 app.use(express.json());
 
-const PORT =process.env.SERVER_PORT | 3000;   
+const PORT = process.env.SERVER_PORT | 3000;
 
-app.get('/', (req,resp)=> {
-    resp.status(200).json({"message":"Connected..."})
+app.get('/', (req, resp) => {
+    resp.status(200).json({ "message": "Connected..." })
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server Started and Running on port ${PORT}`)
 })
---
+++ routes/customerRoutes.js
@@ -0,0 +1,11 @@
+const express= require ('express');
+const router=express.Router();
+const customerController = require('../controller/customerController');
+
+router.get('/', customerController.getAllCustomers);
+router.get('/:id', customerController.getCustomerById);
+router.post('/', customerController.createNewCustomer);
+router.put('/:id', customerController.updateCustomer);
+router.delete('/:id', customerController.getAllCustomers);
+
+module.export=router;
